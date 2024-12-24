9d9838400a2aa1c1e4131ecf17debda22b9a23bf
hryhorii <grishkoshkarta1@gmail.com>
2024-12-24 19:56:41 +0200

    change prices

--- src/button/index.tsx
+++ src/button/index.tsx
@@ -3,9 +3,7 @@ import s from "./styles.module.scss";
 
 const Button: FC = () => (
 	<button className={s.button} type={"button"}>
-		<a href="https://t.me/ys_department_of_care_bot?start=6742f1d3e01c8aca7d035e5f">
-			Придбати
-		</a>
+		<a href="https://t.me/ysvocalcoach">Придбати</a>
 	</button>
 );
 
